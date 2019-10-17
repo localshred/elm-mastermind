294549fd89be875b771d4680bd37acc868dad3f8
BJ Neilsen <bj.neilsen@gmail.com>
2019-10-17 11:55:31 -0400

    Lots of style updates

--- src/View.elm
+++ src/View.elm
@@ -1,9 +1,9 @@
 module View exposing (..)
 
 import Dict exposing (toList)
-import Html exposing (Html, button, div, h2, input, label, text, span)
+import Html exposing (Html, button, div, h2, h3, input, label, text, span)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (classList, disabled, style, type_, value)
+import Html.Attributes exposing (classList, disabled, id, type_, value)
 import Types exposing (..)
 
 
@@ -13,44 +13,22 @@ view model =
         innerGameView =
             case model.gameState of
                 GameNew ->
-                    newGameView model
+                    viewGameNew model
 
-                GamePlaying _ ->
-                    playingGameView model
-
-                GameWon ->
-                    playingGameView model
-
-                GameLost ->
-                    playingGameView model
+                _ ->
+                    viewGamePlaying model
     in
-        div
-            [ style "width" "50%"
-            , style "margin" "0 auto"
-            , style "background" "#f1eed3"
-            ]
-            ([ h2 [] [ text "Mastermind" ] ]
+        div [ id "main" ] <|
+            [ h2 [ id "game-title" ] [ text "Mastermind" ] ]
                 ++ [ innerGameView ]
-            )
-
-
-gameWonView : Model -> Html Msg
-gameWonView model =
-    div [] [ text "you won the game" ]
-
-
-gameLostView : Model -> Html Msg
-gameLostView model =
-    div [] [ text "you lost the game" ]
 
 
 textInput : String -> String -> (String -> Msg) -> Html Msg
 textInput labelText inputValue inputHandler =
-    div
-        []
+    inputFieldRow
         [ label [] [ text labelText ]
         , input
-            [ type_ "text"
+            [ type_ "number"
             , value inputValue
             , onInput inputHandler
             ]
@@ -58,49 +36,53 @@ textInput labelText inputValue inputHandler =
         ]
 
 
-newGameView : Model -> Html Msg
-newGameView model =
+inputFieldRow : List (Html Msg) -> Html Msg
+inputFieldRow children =
+    div
+        [ classList [ ( "input-field-row", True ) ] ]
+        children
+
+
+viewGameNew : Model -> Html Msg
+viewGameNew model =
     let
         isStartGameEnabled =
-            not (model.totalRounds > 0 && model.totalSlots > 0)
+            not <| model.totalRounds > 0 && model.totalSlots > 0
     in
         div
-            []
-            [ div [] [ textInput "total rounds" (String.fromInt model.totalRounds) ConfigSetTotalRounds ]
-            , div [] [ textInput "total slots" (String.fromInt model.totalSlots) ConfigSetTotalSlots ]
-            , div []
-                [ button
-                    [ onClick StartGame
-                    , disabled isStartGameEnabled
-                    ]
-                    [ text "new game" ]
-                ]
+            [ classList [ ( "new-game", True ) ] ]
+            [ inputFieldRow <| [ textInput "# Rounds" (String.fromInt model.totalRounds) ConfigSetTotalRounds ]
+            , inputFieldRow <| [ textInput "# Pegs" (String.fromInt model.totalSlots) ConfigSetTotalSlots ]
+            , inputFieldRow <|
+                [ btn "Crack the Code!" "grey" StartGame [ disabled isStartGameEnabled ] ]
             ]
 
 
-playingGameView : Model -> Html Msg
-playingGameView model =
-    div
-        []
-        [ displayGameStateTitle model
-        , roundsView model
-        , h2 [] [ text "Solution" ]
-        , solutionView model
-        ]
+maybeToListItem : Maybe a -> List a
+maybeToListItem maybe =
+    maybe |> Maybe.map List.singleton |> Maybe.withDefault []
 
 
-pegView : String -> String -> List (Html.Attribute Msg) -> Html Msg
-pegView borderStyle backgroundColor additionalAttrs =
+viewGamePlaying : Model -> Html Msg
+viewGamePlaying model =
+    let
+        solution =
+            model |> solutionView |> maybeToListItem
+    in
+        div [] <|
+            [ displayGameStateTitle model ]
+                ++ solution
+                ++ [ roundsView model ]
+
+
+pegView : String -> List (Html.Attribute Msg) -> Html Msg
+pegView pegClassName additionalAttrs =
     div
-        ([ style "border" "2px solid #333"
-         , style "border-radius" "50%"
-         , style "border-style" borderStyle
-         , style "background" backgroundColor
-         , style "display" "inline-block"
-         , style "width" "25px"
-         , style "height" "25px"
-         , style "overflow" "hidden"
-         , style "margin" "5px 10px"
+        ([ classList
+            [ ( "peg", True )
+            , ( pegClassName, True )
+            , ( "pegged", pegClassName /= "clear" )
+            ]
          ]
             ++ additionalAttrs
         )
@@ -110,14 +92,11 @@ pegView borderStyle backgroundColor additionalAttrs =
 slotView : RoundNumber -> Model -> ( SlotNumber, Slot ) -> Html Msg
 slotView roundNumber model ( slotNumber, ( peg, pip ) ) =
     let
-        borderStyle =
-            pegBorderStyle peg
-
-        backgroundColor =
-            pegColor peg
+        pegClassName =
+            pegClassColor peg
 
         currentRoundSlotAttrs =
-            [ style "cursor" "pointer"
+            [ classList [ ( "current-round", True ) ]
             , onClick (SelectSlot roundNumber slotNumber)
             ]
 
@@ -125,106 +104,112 @@ slotView roundNumber model ( slotNumber, ( peg, pip ) ) =
             model.currentRound == roundNumber
     in
         if isCurrentRound then
-            pegView borderStyle backgroundColor currentRoundSlotAttrs
+            pegView pegClassName currentRoundSlotAttrs
         else
-            pegView borderStyle backgroundColor []
+            pegView pegClassName []
 
 
-roundView : Model -> RoundNumber -> Slots -> List (Html Msg) -> List (Html Msg)
-roundView model roundNumber slots accumulator =
-    let
-        roundDiv =
-            div
-                [ style "display" "flex"
-                , style "flex-direction" "row"
-                , style "justify-content" "space-between"
-                , style "align-items" "center"
-                , style "width" "100%"
-                ]
-                ([ roundNameView roundNumber
-                 , slotsView roundNumber slots model
-                 , roundInfoView roundNumber model
-                 ]
-                )
-    in
-        accumulator ++ [ roundDiv ]
+roundView : Model -> ( RoundNumber, Slots ) -> Html Msg
+roundView model ( roundNumber, slots ) =
+    div
+        [ classList [ ( "round", True ) ] ]
+        [ roundNameView roundNumber
+        , slotsView roundNumber slots model
+        , roundInfoView roundNumber model
+        ]
+
+
+isPickerOpen : Model -> Bool
+isPickerOpen model =
+    case model.gameState of
+        GamePlaying (PegPickerOpen _ _) ->
+            True
+
+        _ ->
+            False
 
 
 slotsView : RoundNumber -> Slots -> Model -> Html Msg
 slotsView roundNumber slots model =
     div
-        [ style "width" "60%"
-        , style "position" "relative"
+        [ classList
+            [ ( "slots", True )
+            , ( "picker-open", isPickerOpen model )
+            ]
         ]
         (slots
             |> Dict.toList
             |> List.map (slotView roundNumber model)
-            |> List.append [ pegPickerView roundNumber model ]
+            |> List.append
+                (model
+                    |> pegPickerView roundNumber
+                    |> maybeToListItem
+                )
         )
 
 
-pegPickerView : RoundNumber -> Model -> Html Msg
+pegPickerView : RoundNumber -> Model -> Maybe (Html Msg)
 pegPickerView roundNumber model =
     case model.gameState of
         GamePlaying (PegPickerOpen selectedRoundNumber selectedSlotNumber) ->
             if model.currentRound == roundNumber then
-                div
-                    [ style "background" "#aaa"
-                    , style "position" "absolute"
-                    , style "top" "50px"
-                    , style "left" "125px"
-                    , style "width" "150px"
-                    , style "z-index" "1"
-                    ]
-                    ([ span [] [ text "Pick Color" ] ] ++ (pegPickerSlotsView selectedRoundNumber selectedSlotNumber))
+                Just <| pegPickerSlotsView selectedRoundNumber selectedSlotNumber
             else
-                span [] []
+                Nothing
 
         _ ->
-            span [] []
+            Nothing
 
 
-pegPickerSlotsView : RoundNumber -> SlotNumber -> List (Html Msg)
+pegPickerSlotsView : RoundNumber -> SlotNumber -> Html Msg
 pegPickerSlotsView roundNumber slotNumber =
     let
         pegSlotView peg =
-            pegView (pegBorderStyle peg) (pegColor peg) [ onClick <| SetPeg roundNumber slotNumber peg ]
-
-        slots =
-            allPegs
-                |> List.map pegSlotView
+            pegView (pegClassColor peg) [ onClick <| SetPeg roundNumber slotNumber peg ]
     in
-        [ div [] slots ]
+        div
+            [ classList [ ( "peg-picker", True ) ] ]
+            (allPegs |> List.map pegSlotView)
+
+
+btn : String -> String -> Msg -> List (Html.Attribute Msg) -> Html Msg
+btn txt color msg attrs =
+    button
+        ([ classList [ ( "btn-" ++ color, True ) ]
+         , onClick msg
+         ]
+            ++ attrs
+        )
+        [ text txt ]
 
 
 roundInfoView : RoundNumber -> Model -> Html Msg
 roundInfoView roundNumber model =
     let
+        buttonChild : Maybe (Html Msg)
         buttonChild =
             case model.gameState of
                 GamePlaying _ ->
-                    button
-                        [ onClick CommitRound
-                        , disabled <| (areRoundSlotsFull roundNumber model |> not)
-                        ]
-                        [ text "Check My Guess" ]
+                    if areRoundSlotsFull roundNumber model then
+                        Just <|
+                            btn "COMPUTE" "grey" CommitRound []
+                    else
+                        Nothing
 
                 _ ->
-                    span [] []
+                    Nothing
 
+        children : List (Html Msg)
         children =
             if roundNumber == model.currentRound then
-                [ buttonChild
-                ]
+                buttonChild |> maybeToListItem
             else if roundNumber < model.currentRound then
-                pipsForRoundView roundNumber model
+                model |> pipsForRoundView roundNumber
             else
                 []
     in
         div
-            [ style "margin-top" "8px"
-            , style "width" "20%"
-            ]
+            [ classList [ ( "round-info", True ) ] ]
             children
 
 
@@ -242,102 +227,85 @@ pipsForRoundView roundNumber model =
                 |> Dict.values
                 |> List.map pipView
     in
-        [ div [ style "width" "48px" ] pips ]
+        [ div [ classList [ ( "pips", True ) ] ] pips ]
 
 
 pipView : Pip -> Html Msg
 pipView pip =
-    let
-        backgroundColor =
-            case pip of
-                PipNoMatch ->
-                    "#666"
-
-                PipColorMatch ->
-                    "#fff"
-
-                PipColorSlotMatch ->
-                    "#f00"
-    in
-        span
-            [ style "border" "1px solid #333"
-            , style "border-radius" "50%"
-            , style "border-style" "solid"
-            , style "background" backgroundColor
-            , style "display" "inline-block"
-            , style "width" "10px"
-            , style "height" "10px"
-            , style "overflow" "hidden"
-            , style "margin" "3px"
+    span
+        [ classList
+            [ ( "pip", True )
+            , ( "no-match", pip == PipNoMatch )
+            , ( "color-match", pip == PipColorMatch )
+            , ( "color-slot-match", pip == PipColorSlotMatch )
             ]
-            []
+        ]
+        []
 
 
 roundNameView : RoundNumber -> Html Msg
 roundNameView roundNumber =
     let
         roundName =
-            if roundNumber == 0 then
-                ""
-            else
-                "Round " ++ String.fromInt roundNumber
+            case roundNumber of
+                0 ->
+                    "Solution"
+
+                _ ->
+                    String.fromInt roundNumber
     in
         div
-            [ style "padding-top" "8px"
-            , style "width" "20%"
-            ]
+            [ classList [ ( "round-name", True ) ] ]
             [ text roundName ]
 
 
-solutionView : Model -> Html Msg
+solutionView : Model -> Maybe (Html Msg)
 solutionView model =
-    div
-        [ style "padding" "5px"
-        , style "margin" "5px"
-        , style "text-align" "center"
-        ]
-        (roundView model 0 model.solution [])
+    case model.gameState of
+        GamePlaying _ ->
+            Nothing
+
+        GameNew ->
+            Nothing
+
+        _ ->
+            Just <|
+                div [ classList [ ( "solution", True ) ] ]
+                    [ roundView model ( 0, model.solution ) ]
 
 
 roundsView : Model -> Html Msg
 roundsView model =
-    div
-        [ style "width" "100%"
-        , style "padding" "5px"
-        , style "margin" "5px"
-        , style "text-align" "center"
-        , style "display" "flex"
-        , style "flex-direction" "column"
-        , style "justify-content" "space-between"
-        , style "align-items" "center"
-        ]
-        (Dict.foldl
-            (roundView model)
-            []
-            model.rounds
+    div [ classList [ ( "rounds", True ) ] ]
+        (model.rounds
+            |> Dict.toList
+            |> List.take model.currentRound
+            |> List.map (roundView model)
         )
+
+
+gameEnd : String -> String -> Html Msg
+gameEnd class txt =
+    div [ classList [ ( "game-state-end", True ), ( class, True ) ] ]
+        [ h3 [] [ text txt ]
+        , btn "New Game" "grey" ResetGame []
+        ]
 
 
 displayGameStateTitle : Model -> Html Msg
 displayGameStateTitle model =
     case model.gameState of
         GameNew ->
-            h2 [] [ text "Game hasn't started yet" ]
+            h3 [] [ text "Game hasn't started yet" ]
 
         GamePlaying _ ->
-            h2 [] [ text <| guessesCountDescription model ]
+            h3 [] [ text <| guessesCountDescription model ]
 
         GameWon ->
-            h2 []
-                [ text "Yahoo! You won the game!"
-                , button [ onClick ResetGame ] [ text "New Game" ]
-                ]
+            gameEnd "game-state-won" <| "You won the game in " ++ String.fromInt model.currentRound ++ " Rounds!"
 
         GameLost ->
-            h2 []
-                [ text "Sorry, you lost the game!"
-                , button [ onClick ResetGame ] [ text "New Game" ]
-                ]
+            gameEnd "game-state-lost" "Sorry, you lost the game!"
 
 
 guessesCountDescription : Model -> String
@@ -352,36 +320,26 @@ guessesCountDescription model =
         "Round " ++ currentRound ++ " of " ++ totalRounds
 
 
-pegBorderStyle : Peg -> String
-pegBorderStyle peg =
-    case peg of
-        PegNone ->
-            "dotted"
-
-        _ ->
-            "solid"
-
-
-pegColor : Peg -> String
-pegColor peg =
+pegClassColor : Peg -> String
+pegClassColor peg =
     case peg of
         PegGreen ->
-            "#0f0"
+            "green"
 
         PegYellow ->
             "yellow"
 
         PegBlack ->
-            "#000"
+            "black"
 
         PegWhite ->
-            "#fff"
+            "white"
 
         PegBlue ->
-            "#00f"
+            "blue"
 
         PegRed ->
-            "#f00"
+            "red"
 
         PegNone ->
             "clear"
